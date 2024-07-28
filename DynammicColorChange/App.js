95f4a147737a09d26e62681f9397a4df82ad709e
Ramasnhu Yadav <ramanshu8860@gmail.com>
2024-07-28 12:32:51 +0530

    secondCommmitwithoutDynamiicButtons

--- DynammicColorChange/App.js
+++ DynammicColorChange/App.js
@@ -1,7 +1,23 @@
-function changeColor(color){
-    const dBox = document.querySelector(box);
-    dBox.style.background = color;
+function changeColor(color) {
+  let DynammicBox = document.getElementById("DynammicBox");
+  DynammicBox.style.background = color;
 }
 
-const buttons = document.querySelector(buttons);
-buttons.addEventListener('onclick', changeColor)
+var colors = ["maroon","darkGreen"];
+let outputDiv = document.getElementById("dynamicallyCreated");
+for (var i = 0; i < colors.length; i++) {
+  var createButton = document.createElement("button");
+  createButton.innerText = colors[i];
+  createButton.classList.add("button");
+  createButton.style.margin = "2px";
+  createButton.style.padding = "2px";
+  createButton.style.backgroundColor = colors[i];
+
+  (function (color) {
+    console.log("color is: ", color);
+    item.onclick = () => {
+      return changeColor(color);
+    };
+  })(colors[i]);
+  outputDiv.appendChild(createButton);
+}
